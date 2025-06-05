bcd53940ea84a98e145d9d76aa31519011f2577e
Henishma420 <henishma645@gmail.com>
2025-06-05 21:40:49 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,59 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript { // Add this block
-    repositories {
-        google()
-        mavenCentral()
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") // Add Google Services plugin
+}
+
+android {
+    namespace = "com.eles.traffic_pro_java"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.eles.traffic_pro_java"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0") // Or the latest version
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
-plugins {
-    alias(libs.plugins.android.application) apply false
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0")) // Add Firebase BOM
+    implementation("com.google.firebase:firebase-database") // Add Firebase Database
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(libs.material.v190)
+    implementation(libs.play.services.location)
+    implementation(libs.osmdroid.android)
+    implementation(libs.okhttp)
+    implementation(libs.appcompat.v161)
+    implementation(libs.fragment)
+    implementation(libs.osmdroid.android.v6114)
 }
